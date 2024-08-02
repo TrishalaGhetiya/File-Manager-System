235999d7d73f4e5aa1b83d9fb14da3d9fa92575e
Trishala Ghetiya <trishalaghetiya@gmail.com>
2024-08-02 17:41:12 +0530

    user signup and login

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+require('dotenv').config();
+//const fileRoutes = require('./routes/fileRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-
 app.use(express.json());
 
-app.listen(3000, ()=> {
-    console.log("Server running");
-})
+app.use('/user', userRoutes);
+//app.use('/files', fileRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const {User} = require('../models');
+require('dotenv').config();
+
+exports.signup = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const user = await User.create({ username, email, password });
+        res.status(201).json({ message: 'User signed up successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ where: { email } });
+        if (!user) return res.status(401).json({ error: 'Invalid email or password' });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(401).json({ error: 'Invalid email or password' });
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ message: 'Login successful', token });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
